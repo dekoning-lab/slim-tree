10820767d5d86bae6fce0755f1518e66579a8853
ebrintn <e.brintnell@outlook.com>
2025-05-17 19:26:35 -0600

    Fixed minor bug

--- utils/writeSLiMHPC.py
+++ utils/writeSLiMHPC.py
@@ -210,9 +210,9 @@ class writeSLiMHPC(writeSLiM):
 
 
         #If this is the last clade from a certain parent, write script to destroy that parent's temporary files
-        #if(population_parameters["last_child_clade"]):
-        #    end_population_string += ("\n\n\tsystem(\"rm " + population_parameters["parent_pop_name"] + ".txt\");" +
-        #                              "\n\tsystem(\"rm " + population_parameters["parent_pop_name"] + ".fasta\");")
+        if(population_parameters["last_child_clade"]):
+           end_population_string += ("\n\n\tsystem(\"rm " + population_parameters["parent_pop_name"] + ".txt\");" +
+                                     "\n\tsystem(\"rm " + population_parameters["parent_pop_name"] + ".fasta\");")
 
 
         
